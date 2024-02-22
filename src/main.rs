f4eeaa23631682f913d6792ad580ac896d78afcf
Aryan Sjet <sjet@asjet.dev>
2024-02-22 22:22:38 +0800

    test: add cargo unit test support

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![allow(dead_code)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
 mod vga;
+mod asm;
 
 const PANIC_INFO_COLOR: vga::ColorCode = vga::ColorCode::new(vga::Color::LightRed, vga::Color::Black);
 
 /// This function is called on panic.
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    vga::WRITER.lock().set_color(PANIC_INFO_COLOR);
+    vga::set_color(PANIC_INFO_COLOR);
     println!("{}", info);
     loop {}
 }
@@ -21,5 +26,36 @@ pub extern "C" fn _start() -> ! {
     // named `_start` by default
     println!("Hello, World!\ninteger: {}, float: {}", 42, 3.14);
 
-    panic!("Some panic message");
+    // panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+const TEST_NORM_COLOR: vga::ColorCode = vga::ColorCode::new(vga::Color::White, vga::Color::Black);
+const TEST_INIT_COLOR: vga::ColorCode = vga::ColorCode::new(vga::Color::LightCyan, vga::Color::Black);
+const TEST_PASS_COLOR: vga::ColorCode = vga::ColorCode::new(vga::Color::LightGreen, vga::Color::Black);
+const TEST_FAIL_COLOR: vga::ColorCode = vga::ColorCode::new(vga::Color::LightRed, vga::Color::Black);
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    vga::set_color(TEST_INIT_COLOR);
+    println!("Running {} tests", tests.len());
+    vga::set_color(TEST_NORM_COLOR);
+    for test in tests {
+        test();
+    }
+
+    asm::exit_qemu(asm::QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    vga::set_color(TEST_PASS_COLOR);
+    println!("[ok]");
+    vga::set_color(TEST_NORM_COLOR);
 }
