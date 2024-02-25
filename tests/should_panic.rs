fa7c13d9d1eb70b0bd0fe691d06faa2125492363
Aryan Sjet <sjet@asjet.dev>
2024-02-25 17:38:53 +0800

    chore: disable harness for panic test

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,17 +1,16 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use xv6::asm;
-use xv6::serial_println;
-use xv6::test::Testable;
+use xv6::{serial_print, serial_println};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    serial_print!("should_panic::should_panic...\t");
+    should_panic();
+    serial_println!("[test did not panic]");
+    asm::exit_qemu(asm::QemuExitCode::Failure);
     loop {}
 }
 
@@ -22,17 +21,6 @@ pub fn panic(_: &PanicInfo) -> ! {
     loop {}
 }
 
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-        serial_println!("[test did not panic]");
-        asm::exit_qemu(asm::QemuExitCode::Failure);
-    }
-    asm::exit_qemu(asm::QemuExitCode::Success);
-}
-
-#[test_case]
 fn should_panic() {
     assert_eq!(0, 1);
 }
