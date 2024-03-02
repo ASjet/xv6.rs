d275167f172ba16b6aa487c29df33bd031d35420
Aryan Sjet <sjet@asjet.dev>
2024-03-02 21:12:09 +0800

    feat(asm): specify interrupt stack for double fault handler

--- src/asm/x86_64.rs
+++ src/asm/x86_64.rs
@@ -1,3 +1,4 @@
+use super::gdt;
 use super::QemuExitCode;
 use crate::{println, vga, with_color};
 use lazy_static::lazy_static;
@@ -11,7 +12,11 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.double_fault.set_handler_fn(double_fault_handler);
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
         idt
     };
 }
