d15c803c52467c613b519434d06ca06198cdd3fd
Aryan Sjet <sjet@asjet.dev>
2024-03-10 15:36:33 +0800

    feat(bootloader): pass boot info into kernel

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,38 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
+use bootloader::BootInfo;
 use core::panic::PanicInfo;
 use xv6::arch;
+use xv6::dmesg;
 use xv6::println;
 use xv6::vga::{self, Color, ColorCode};
 
 const PANIC_INFO_COLOR: ColorCode = ColorCode::new(Color::LightRed, Color::Black);
 
 #[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start(boot_info: &'static BootInfo) -> ! {
     xv6::init();
 
-    // x86_64::instructions::interrupts::int3();
+    dmesg!(
+        "physical memory offset: {:#x}",
+        boot_info.physical_memory_offset
+    );
+    for &region in boot_info.memory_map.iter() {
+        dmesg!(
+            "start: {:#x}, end: {:#x}, type: {:?}",
+            region.range.start_addr(),
+            region.range.end_addr(),
+            region.region_type
+        );
+    }
 
     #[cfg(test)]
     test_main();
 
-    unsafe {
-        *(0xdeadbeef as *mut u8) = 42;
-    };
+    // unsafe {
+    //     *(0xdeadbeef as *mut u8) = 42;
+    // };
 
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
