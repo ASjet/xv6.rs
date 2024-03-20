e6686986a883e7c3b37fbf457fa908c14a80f613
Aryan Sjet <sjet@asjet.dev>
2024-03-20 20:43:01 +0800

    docs(creates/rv64): pass doc to `insn::csr::csr_reg` macro

--- crates/rv64/src/insn/csr.rs
+++ crates/rv64/src/insn/csr.rs
@@ -3,7 +3,8 @@ use core::arch::asm;
 use int_enum::IntEnum;
 
 macro_rules! csr_reg {
-    ($reg:ident) => {
+    ($(#[$m:meta])* $reg:ident) => {
+        $(#[$m])*
         #[allow(non_camel_case_types)]
         pub struct $reg;
 
@@ -32,10 +33,15 @@ pub enum PrivilegeLevel {
     M = 0b11,
 }
 
-csr_reg!(mhartid);
+csr_reg!(
+    /// Current hart id
+    mhartid
+);
 
-// Machine mode status
-csr_reg!(mstatus);
+csr_reg!(
+    /// Machine-mode Status Register
+    mstatus
+);
 pub const MSTATUS_TSR: Mask = Mask::new(1, 22);
 pub const MSTATUS_TW: Mask = Mask::new(1, 21);
 pub const MSTATUS_TVM: Mask = Mask::new(1, 20);
@@ -44,27 +50,31 @@ pub const MSTATUS_SUM: Mask = Mask::new(1, 18);
 pub const MSTATUS_MPRV: Mask = Mask::new(1, 17);
 pub const MSTATUS_XS: Mask = Mask::new(2, 15);
 pub const MSTATUS_FS: Mask = Mask::new(2, 13);
-pub const MSTATUS_MPP: Mask = Mask::new(2, 11); // previous privilege mode.
+pub const MSTATUS_MPP: Mask = Mask::new(2, 11); // Machine-mode Previous Privilege
 pub const MSTATUS_VS: Mask = Mask::new(2, 9);
-pub const MSTATUS_SPP: Mask = Mask::new(1, 8);
+pub const MSTATUS_SPP: Mask = Mask::new(1, 8); // Supervisor Previous Privilege
 pub const MSTATUS_MPIE: Mask = Mask::new(1, 7);
 pub const MSTATUS_UBE: Mask = Mask::new(1, 6);
-pub const MSTATUS_SPIE: Mask = Mask::new(1, 5);
-pub const MSTATUS_MIE: Mask = Mask::new(1, 3); // machine-mode interrupt enable.
-pub const MSTATUS_SIE: Mask = Mask::new(1, 1); // supervisor-mode interrupt enable.
+pub const MSTATUS_SPIE: Mask = Mask::new(1, 5); // Supervisor Previous Interrupt Enable
+pub const MSTATUS_MIE: Mask = Mask::new(1, 3); // Machine-mode Interrupt Enable
+pub const MSTATUS_SIE: Mask = Mask::new(1, 1); // Supervisor Interrupt Enable
 
+/// Read mstatus.MPP
 #[inline]
 pub fn r_mstatus_mpp() -> PrivilegeLevel {
     PrivilegeLevel::try_from(mstatus.read_mask(MSTATUS_MPP) as u8 & 0b11).unwrap()
 }
 
+/// Write mstatus.MPP
 #[inline]
 pub unsafe fn w_mstatus_mpp(l: PrivilegeLevel) {
     unsafe { mstatus.write_mask(MSTATUS_MPP, l as u64) }
 }
 
-// Supervisor mode status
-csr_reg!(sstatus);
+csr_reg!(
+    /// Supervisor Status Register
+    sstatus
+);
 pub const SSTATUS_SD: Mask = Mask::new(1, 63);
 pub const SSTATUS_UXL: Mask = Mask::new(2, 32);
 pub const SSTATUS_MXR: Mask = Mask::new(1, 19);
@@ -72,13 +82,15 @@ pub const SSTATUS_SUM: Mask = Mask::new(1, 18);
 pub const SSTATUS_XS: Mask = Mask::new(2, 15);
 pub const SSTATUS_FS: Mask = Mask::new(2, 13);
 pub const SSTATUS_VS: Mask = Mask::new(2, 9);
-pub const SSTATUS_SPP: Mask = Mask::new(1, 8);
+pub const SSTATUS_SPP: Mask = Mask::new(1, 8); // Previous mode, 1=Supervisor, 0=User
 pub const SSTATUS_UBE: Mask = Mask::new(1, 6);
-pub const SSTATUS_SPIE: Mask = Mask::new(1, 5);
-pub const SSTATUS_SIE: Mask = Mask::new(1, 1); // supervisor-mode interrupt enable.
+pub const SSTATUS_SPIE: Mask = Mask::new(1, 5); // Supervisor Previous Interrupt Enable
+pub const SSTATUS_SIE: Mask = Mask::new(1, 1); // Supervisor Interrupt Enable
 
-// Machine Interrupt Pending
-csr_reg!(mip);
+csr_reg!(
+    /// Machine-mode Interrupt Pending
+    mip
+);
 pub const MIP_MEIP: Mask = Mask::new(1, 11); // external
 pub const MIP_SEIP: Mask = Mask::new(1, 9); // external
 pub const MIP_MTIP: Mask = Mask::new(1, 7); // timer
@@ -86,34 +98,60 @@ pub const MIP_STIP: Mask = Mask::new(1, 5); // timer
 pub const MIP_MSIP: Mask = Mask::new(1, 3); // software
 pub const MIP_SSIP: Mask = Mask::new(1, 1); // software
 
-// Supervisor Interrupt Pending
-csr_reg!(sip);
+csr_reg!(
+    /// Supervisor Interrupt Pending
+    sip
+);
 pub const SIP_SEIP: Mask = Mask::new(1, 9); // external
 pub const SIP_STIP: Mask = Mask::new(1, 5); // timer
 pub const SIP_SSIP: Mask = Mask::new(1, 1); // software
 
-// Machine Interrupt Enable
-csr_reg!(mie);
+csr_reg!(
+    /// Machine-mode Interrupt Enable
+    mie
+);
 pub const MIE_SEIE: Mask = Mask::new(1, 11); // external
 pub const MIE_MTIE: Mask = Mask::new(1, 9); // timer
 pub const MIE_STIE: Mask = Mask::new(1, 7); // timer
 pub const MIE_MSIE: Mask = Mask::new(1, 5); // software
 pub const MIE_SSIE: Mask = Mask::new(1, 3); // software
 
-// Supervisor Interrupt Enable
-csr_reg!(sie);
+csr_reg!(
+    /// Supervisor Interrupt Enable
+    sie
+);
 pub const SIE_SEIE: Mask = Mask::new(1, 9); // external
 pub const SIE_STIE: Mask = Mask::new(1, 5); // timer
 pub const SIE_SSIE: Mask = Mask::new(1, 1); // software
 
-csr_reg!(mepc);
+csr_reg!(
+    /// Machine exception program counter, holds the instruction
+    /// address to which a return from exception will go.
+    mepc
+);
 
-csr_reg!(sepc);
+csr_reg!(
+    /// Supervisor exception program counter, holds the instruction
+    /// address to which a return from exception will go.
+    sepc
+);
 
-csr_reg!(medeleg);
+csr_reg!(
+    /// Machine Exception Delegation
+    medeleg
+);
 
-csr_reg!(mideleg);
+csr_reg!(
+    /// Machine Interrupt Delegation
+    mideleg
+);
 
-csr_reg!(mtvec);
+csr_reg!(
+    /// Machine-mode interrupt vector
+    mtvec
+);
 
-csr_reg!(stvec);
+csr_reg!(
+    /// Supervisor Trap-Vector Base Address low two bits are mode.
+    stvec
+);
