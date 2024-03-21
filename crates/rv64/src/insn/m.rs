54b19e66a973e0658af584c3b0be04b69edd3c37
Aryan Sjet <sjet@asjet.dev>
2024-03-21 11:49:05 +0800

    feat(rv64/insn): add more M level registers

--- crates/rv64/src/insn/m.rs
+++ crates/rv64/src/insn/m.rs
@@ -1,13 +1,37 @@
 use super::{Mask, PrivilegeLevel, RegisterRW};
 use crate::{csr_reg_ro, csr_reg_rw};
 
+/*            Machine Information Register            */
+
 csr_reg_ro!(
-    /// Current hart id
+    /// Vendor ID
+    mvendorid
+);
+
+csr_reg_ro!(
+    /// Architecture ID
+    marchid
+);
+
+csr_reg_ro!(
+    /// Implementation ID
+    mimpid
+);
+
+csr_reg_ro!(
+    /// Hardware thread ID
     mhartid
 );
 
+csr_reg_ro!(
+    /// Pointer to configuration data structure
+    mconfigptr
+);
+
+/*            Machine Trap Setup            */
+
 csr_reg_rw!(
-    /// Machine-mode Status Register
+    /// Machine status register
     mstatus
 );
 impl mstatus {
@@ -41,7 +65,64 @@ pub const MSTATUS_MIE: Mask = Mask::new(1, 3); // Machine-mode Interrupt Enable
 pub const MSTATUS_SIE: Mask = Mask::new(1, 1); // Supervisor Interrupt Enable
 
 csr_reg_rw!(
-    /// Machine-mode Interrupt Pending
+    /// ISA and extensions
+    misa
+);
+
+csr_reg_rw!(
+    /// Machine exception delegation register
+    medeleg
+);
+
+csr_reg_rw!(
+    /// Machine interrupt delegation register
+    mideleg
+);
+
+csr_reg_rw!(
+    /// Machine interrupt-enable register
+    mie
+);
+pub const MIE_SEIE: Mask = Mask::new(1, 11); // external
+pub const MIE_MTIE: Mask = Mask::new(1, 9); // timer
+pub const MIE_STIE: Mask = Mask::new(1, 7); // timer
+pub const MIE_MSIE: Mask = Mask::new(1, 5); // software
+pub const MIE_SSIE: Mask = Mask::new(1, 3); // software
+
+csr_reg_rw!(
+    /// Machine trap-handler base address
+    mtvec
+);
+
+csr_reg_rw!(
+    /// Machine counter enable
+    mcounteren
+);
+
+/*            Machine Trap Handling            */
+
+csr_reg_rw!(
+    /// Scratch register for machine trap handlers
+    mscratch
+);
+
+csr_reg_rw!(
+    /// Machine exception program counter
+    mepc
+);
+
+csr_reg_rw!(
+    /// Machine trap cause
+    mcause
+);
+
+csr_reg_rw!(
+    /// Machine bad address or instruction
+    mtval
+);
+
+csr_reg_rw!(
+    /// Machine interrupt pending
     mip
 );
 pub const MIP_MEIP: Mask = Mask::new(1, 11); // external
@@ -52,46 +133,78 @@ pub const MIP_MSIP: Mask = Mask::new(1, 3); // software
 pub const MIP_SSIP: Mask = Mask::new(1, 1); // software
 
 csr_reg_rw!(
-    /// Machine-mode Interrupt Enable
-    mie
-);
-pub const MIE_SEIE: Mask = Mask::new(1, 11); // external
-pub const MIE_MTIE: Mask = Mask::new(1, 9); // timer
-pub const MIE_STIE: Mask = Mask::new(1, 7); // timer
-pub const MIE_MSIE: Mask = Mask::new(1, 5); // software
-pub const MIE_SSIE: Mask = Mask::new(1, 3); // software
-
-csr_reg_rw!(
-    /// Machine exception program counter, holds the instruction
-    /// address to which a return from exception will go.
-    mepc
+    /// Machine trap instruction (transformed)
+    mtinst
 );
 
 csr_reg_rw!(
-    /// Machine Exception Delegation
-    medeleg
+    /// Machine bad guest physical address
+    mtval2
+);
+
+/*            Machine Configuration            */
+
+csr_reg_rw!(
+    /// Machine environment configuration register
+    menvcfg
 );
 
 csr_reg_rw!(
-    /// Machine Interrupt Delegation
-    mideleg
+    /// Machine security configuration register
+    mseccfg
+);
+
+/*            Machine Memory Protection            */
+
+csr_reg_rw!(
+    /// Physical memory protection configuration
+    pmpcfg0
+);
+// ...
+
+csr_reg_rw!(
+    /// Physical memory protection address register
+    pmpaddr0
+);
+// ...
+
+/*            Machine Non-Maskable Interrupt Handling            */
+
+csr_reg_rw!(
+    /// Resumable NMI scratch register
+    mnscratch
 );
 
 csr_reg_rw!(
-    /// Machine-mode interrupt vector
-    mtvec
-);
-
-csr_reg_rw!(pmpcfg0);
-
-csr_reg_rw!(pmpaddr0);
-
-csr_reg_rw!(
-    /// Machine-mode Scratch register, for early trap handler
-    mscratch
+    /// Resumable NMI program counter
+    mnepc
 );
 
 csr_reg_rw!(
-    /// Machine-mode Counter-Enable
-    mcounteren
+    /// Resumable NMI cause
+    mncause
+);
+
+csr_reg_rw!(
+    /// Resumable NMI status
+    mnstatus
+);
+
+/*            Machine Counter/Timers            */
+
+csr_reg_rw!(
+    /// Machine cycle counter
+    mcycle
+);
+
+csr_reg_rw!(
+    /// Machine instructions-retired counter
+    minstret
+);
+
+/*            Machine Counter Setup            */
+
+csr_reg_rw!(
+    /// Machine counter-inhibit register
+    mcountinhibit
 );
