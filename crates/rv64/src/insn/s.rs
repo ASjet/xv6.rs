8ad78f66ec0acbb379aa4dda92bd5bca5331b6ec
Aryan Sjet <sjet@asjet.dev>
2024-03-21 11:53:00 +0800

    feat(rv64/insn): add more S level registers

--- crates/rv64/src/insn/s.rs
+++ crates/rv64/src/insn/s.rs
@@ -1,8 +1,10 @@
 use super::Mask;
 use crate::{csr_reg_ro, csr_reg_rw};
 
+/*            Supervisor Trap Setup            */
+
 csr_reg_rw!(
-    /// Supervisor Status Register
+    /// Supervisor status register
     sstatus
 );
 pub const SSTATUS_SD: Mask = Mask::new(1, 63);
@@ -18,15 +20,7 @@ pub const SSTATUS_SPIE: Mask = Mask::new(1, 5); // Supervisor Previous Interrupt
 pub const SSTATUS_SIE: Mask = Mask::new(1, 1); // Supervisor Interrupt Enable
 
 csr_reg_rw!(
-    /// Supervisor Interrupt Pending
-    sip
-);
-pub const SIP_SEIP: Mask = Mask::new(1, 9); // external
-pub const SIP_STIP: Mask = Mask::new(1, 5); // timer
-pub const SIP_SSIP: Mask = Mask::new(1, 1); // software
-
-csr_reg_rw!(
-    /// Supervisor Interrupt Enable
+    /// Supervisor interrupt-enable register
     sie
 );
 pub const SIE_SEIE: Mask = Mask::new(1, 9); // external
@@ -34,38 +28,60 @@ pub const SIE_STIE: Mask = Mask::new(1, 5); // timer
 pub const SIE_SSIE: Mask = Mask::new(1, 1); // software
 
 csr_reg_rw!(
-    /// Supervisor exception program counter, holds the instruction
-    /// address to which a return from exception will go.
-    sepc
-);
-
-csr_reg_rw!(
-    /// Supervisor Trap-Vector Base Address low two bits are mode.
+    /// Supervisor trap handler base address
     stvec
 );
 
+csr_reg_rw!(
+    /// Supervisor counter enable
+    scounteren
+);
+
+/*            Supervisor Configuration            */
+
+csr_reg_rw!(
+    /// Supervisor environment configuration register
+    senvcfg
+);
+
+/*            Supervisor Trap Handling            */
+
+csr_reg_rw!(
+    /// Scratch register for supervisor trap handlers
+    sscratch
+);
+
+csr_reg_rw!(
+    /// Supervisor exception program counter
+    sepc
+);
+
+csr_reg_ro!(
+    /// Supervisor trap cause
+    scause
+);
+
+csr_reg_ro!(
+    /// Supervisor bad address or instruction
+    stval
+);
+
+csr_reg_rw!(
+    /// Supervisor interrupt pending
+    sip
+);
+pub const SIP_SEIP: Mask = Mask::new(1, 9); // external
+pub const SIP_STIP: Mask = Mask::new(1, 5); // timer
+pub const SIP_SSIP: Mask = Mask::new(1, 1); // software
+
+/*            Supervisor Protection and Translation            */
+
+csr_reg_rw!(
+    /// Supervisor address translation and protection
+    satp
+);
 /// use riscv's sv39 page table scheme.
 pub const SATP_SV39: Mask = Mask::new(1, 63);
 pub const fn make_satp(pagetable: u64) -> u64 {
     SATP_SV39.mask() | (pagetable >> 12)
 }
-csr_reg_rw!(
-    /// Supervisor address translation and protection;
-    /// holds the address of the page table.
-    satp
-);
-
-csr_reg_rw!(
-    /// Supervisor Scratch register, for early trap handler
-    sscratch
-);
-
-csr_reg_ro!(
-    /// Supervisor Trap Cause
-    scause
-);
-
-csr_reg_ro!(
-    /// Supervisor Trap Value
-    stval
-);
