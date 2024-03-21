e54e3ee3c02d26a6a8f786cf1b6eb6f32b2233a8
Aryan Sjet <sjet@asjet.dev>
2024-03-22 00:02:07 +0800

    docs(main):  boot sequence in riscv64

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,59 @@
 #![no_main]
 #![feature(riscv_ext_intrinsics)]
 
+//! ## Boot sequence in riscv64:
+//!
+//! 1. Firmware:
+//!     1. Setup SBI arguments:
+//!         1. `a0`: value of current `mhartid`
+//!         2. `a1`: value at the physical address of `0x1020`(0xbfe00000)
+//!         3. `a2`: where `fw_dynamic_info` located
+//!     2. Jump to `0x80000000`(`_start`) in M privilege mode
+//! 2. `_start`:
+//!     1. Init DWARF call frame information
+//!     2. Setup stack and frame pointer
+//!     3. Clear registers(`MIE`,`MIP`,`X1-X9`,`X13-X31`)
+//!     4. Abort hart with `mhartid`(or `a0`) larger than `_max_hart_id`
+//!     5. Call `_start_rust(a0, a1, a2)`
+//! 3. `_start_rust`:
+//!     1. Call `_mp_hook()`, and only hart got a `true` will do the following:
+//!         1. Call `__pre_init()`
+//!         2. Init RAM
+//!             - Copy over .data from flash to RAM
+//!             - Zero out .bss(where global and static variables located)
+//!     2. Enable fpu in `mstatus` CSR
+//!     3. Zero out floating-point registers
+//!     4. Call `_setup_interrupts()`
+//!     5. Call `main(a0, a1, a2)`.
+//!
+//! ## Symbols can be override:
+//!
+//! ### `_max_hart_id`
+//!
+//! - Default: 0
+//! - Override: define in `boot/memory.x` as `_max_hart_id = <value>;`
+//!
+//! ### `_mp_hook`
+//!
+//! - Default: halt all harts except hart 0
+//! - Override: `#[export_name = "_mp_hook"]`
+//!
+//! ### `__pre_init`
+//!
+//! - Default: does nothing
+//! - Override: `#[pre_init]`
+//!
+//! ### `_setup_interrupts`
+//!
+//! - Default: store `_start_trap` as direct trap mode to `mtvec` CSR, which will do:
+//!     1. Save registers to stack
+//!     2. Call `_start_trap_rust`, which will call default handlers
+//! - Override: `#[export_name = "_setup_interrupts"]`
+//!
+//! ### `main`
+//!
+//! This symbol MUST be defined with `#[entry]` attribute.
+
 use core::panic::PanicInfo;
 use riscv_rt::entry;
 use rv64::insn::{m, u, RegisterRO, RegisterRW};
