14b47b120b4c9a73190de62110dfbd2480629ca3
Aryan Sjet <sjet@asjet.dev>
2024-03-27 16:33:37 +0800

    refactor(io): move mem io abstraction into `io`

--- src/io.rs
+++ src/io.rs
@@ -1,2 +1,30 @@
 pub mod console;
 pub mod uart;
+
+pub struct BaseIO<T>(*mut T);
+
+impl<T> BaseIO<T> {
+    pub const fn new(base: usize) -> BaseIO<T> {
+        return BaseIO(base as *mut T);
+    }
+
+    pub const fn offset(&self, offset: usize) -> IO<T> {
+        return IO::new(unsafe { self.0.offset(offset as isize) });
+    }
+}
+
+pub struct IO<T>(*mut T);
+
+impl<T> IO<T> {
+    pub const fn new(addr: *mut T) -> Self {
+        IO(addr)
+    }
+
+    pub fn read(&self) -> T {
+        unsafe { self.0.read_volatile() }
+    }
+
+    pub fn write(&self, value: T) {
+        unsafe { self.0.write_volatile(value) };
+    }
+}
