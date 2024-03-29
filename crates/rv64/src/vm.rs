1776c0527293480554e566a5aa1123b46bdeca55
Aryan Sjet <sjet@asjet.dev>
2024-03-29 23:30:37 +0800

    feat(rv64/vm): impl sv57 vm schema

--- crates/rv64/src/vm.rs
+++ crates/rv64/src/vm.rs
@@ -2,6 +2,8 @@ use crate::insn::Mask;
 use core::{mem::size_of, ops::Index};
 
 pub mod sv39;
+pub mod sv48;
+pub mod sv57;
 
 /// The offset inside a page frame
 pub const PAGE_OFFSET: Mask = Mask::new(12, 0);
