72b75692f604e841e084619ef66362a9e6078ab5
Aryan Sjet <sjet@asjet.dev>
2024-03-31 23:58:02 +0800

    feat(mem): print available page info

--- src/mem/alloc.rs
+++ src/mem/alloc.rs
@@ -5,21 +5,26 @@ use rv64::vm::{PhysAddr, PAGE_SIZE};
 
 extern "C" {
     #[link_name = "_sheap"]
-    static _heap_start: usize;
+    static _heap_start: u8;
     #[link_name = "_heap_size"]
-    static _heap_size: usize;
+    static _heap_size: u8;
 }
 
 pub static mut ALLOCATOR: LinkListAllocator = LinkListAllocator::default();
 
 pub fn init() {
     unsafe {
-        // FIXME: panic on access _heap_start and _heap_size
-        let start = PhysAddr::from(_heap_start);
-        let end = PhysAddr::from(_heap_start + _heap_size);
+        let start = PhysAddr::from(&_heap_start as *const u8 as usize);
+        let end = start + (&_heap_size as *const u8 as usize);
         println!("init heap: {:?} - {:?}", start, end);
         ALLOCATOR = LinkListAllocator::new(start, end, PAGE_SIZE);
         ALLOCATOR.kfree_range(start, end);
+        let pages = ALLOCATOR.free_pages();
+        println!(
+            "{} pages ({} KiB) available",
+            pages,
+            pages * PAGE_SIZE / 1024
+        );
     }
 }
 
@@ -38,6 +43,7 @@ pub struct LinkListAllocator {
     heap_start: PhysAddr,
     heap_end: PhysAddr,
     page_size: usize,
+    free_pages: Mutex<usize>,
     free_list: Mutex<*mut FreePage>,
 }
 
@@ -47,6 +53,7 @@ impl LinkListAllocator {
             heap_start: PhysAddr::null(),
             heap_end: PhysAddr::null(),
             page_size: 0,
+            free_pages: Mutex::new(0, "free_pages"),
             free_list: Mutex::new(0 as *mut FreePage, "free_list"),
         }
     }
@@ -56,8 +63,13 @@ impl LinkListAllocator {
             heap_start,
             heap_end,
             page_size,
-            free_list: Mutex::new(0 as *mut FreePage, "free_list"),
+            free_pages: Mutex::new(0, "init_free_pages"),
+            free_list: Mutex::new(0 as *mut FreePage, "init_free_list"),
         }
+    }
+
+    pub fn free_pages(&self) -> usize {
+        *self.free_pages.lock()
     }
 
     /// Allocate one 4096-byte page of physical memory.
@@ -70,6 +82,7 @@ impl LinkListAllocator {
             return None;
         }
         *free_list = (*page).next;
+        *self.free_pages.lock() -= 1;
 
         let page = PhysAddr::from(page as usize);
         page.memset(0xAAAAAAAAAAAAAAAAusize, self.page_size);
@@ -82,7 +95,7 @@ impl LinkListAllocator {
     /// initializing the allocator; see kinit above.)
     pub unsafe fn kfree(&self, page: PhysAddr) {
         if page.page_offset() != 0 || page < self.heap_start || page >= self.heap_end {
-            panic!("dealloc: invalid page: {:?}", page);
+            panic!("kfree: invalid page: {:?}", page);
         }
 
         unsafe {
@@ -91,6 +104,7 @@ impl LinkListAllocator {
             let mut free_list = self.free_list.lock();
             (*page).next = *free_list;
             *free_list = page;
+            *self.free_pages.lock() += 1;
         }
     }
 
