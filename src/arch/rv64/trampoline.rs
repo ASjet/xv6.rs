69c8bbc2084cebdf0ded45c626d8b51cb2a01a9a
Aryan Sjet <sjet@asjet.dev>
2024-04-04 23:59:04 +0800

    feat(arch/trampoline): define `TrapFrame`

--- src/arch/rv64/trampoline.rs
+++ src/arch/rv64/trampoline.rs
@@ -143,3 +143,56 @@ userret:
         sret
 "
 );
+
+/// per-process data for the trap handling code in trampoline.S.
+/// sits in a page by itself just under the trampoline page in the
+/// user page table. not specially mapped in the kernel page table.
+/// the sscratch register points here.
+/// uservec in trampoline.S saves user registers in the trapframe,
+/// then initializes registers from the trapframe's
+/// kernel_sp, kernel_hartid, kernel_satp, and jumps to kernel_trap.
+/// usertrapret() and userret in trampoline.S set up
+/// the trapframe's kernel_*, restore user registers from the
+/// trapframe, switch to the user page table, and enter user space.
+/// the trapframe includes callee-saved user registers like s0-s11 because the
+/// return-to-user path via usertrapret() doesn't return through
+/// the entire kernel call stack.
+#[repr(C)]
+pub struct TrapFrame {
+    /*   0 */ kernel_satp: usize, // kernel page table
+    /*   8 */ kernel_sp: usize, // top of process's kernel stack
+    /*  16 */ kernel_trap: usize, // usertrap()
+    /*  24 */ epc: usize, // saved user program counter
+    /*  32 */ kernel_hartid: usize, // saved kernel tp
+    /*  40 */ ra: usize,
+    /*  48 */ sp: usize,
+    /*  56 */ gp: usize,
+    /*  64 */ tp: usize,
+    /*  72 */ t0: usize,
+    /*  80 */ t1: usize,
+    /*  88 */ t2: usize,
+    /*  96 */ s0: usize,
+    /* 104 */ s1: usize,
+    /* 112 */ a0: usize,
+    /* 120 */ a1: usize,
+    /* 128 */ a2: usize,
+    /* 136 */ a3: usize,
+    /* 144 */ a4: usize,
+    /* 152 */ a5: usize,
+    /* 160 */ a6: usize,
+    /* 168 */ a7: usize,
+    /* 176 */ s2: usize,
+    /* 184 */ s3: usize,
+    /* 192 */ s4: usize,
+    /* 200 */ s5: usize,
+    /* 208 */ s6: usize,
+    /* 216 */ s7: usize,
+    /* 224 */ s8: usize,
+    /* 232 */ s9: usize,
+    /* 240 */ s10: usize,
+    /* 248 */ s11: usize,
+    /* 256 */ t3: usize,
+    /* 264 */ t4: usize,
+    /* 272 */ t5: usize,
+    /* 280 */ t6: usize,
+}
