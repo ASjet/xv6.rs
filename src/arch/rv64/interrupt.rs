6785eb1cbb34514afe1fab253a1bade44d103809
Aryan Sjet <sjet@asjet.dev>
2024-04-05 12:57:34 +0800

    refactor(arch/trap): update code structure

--- src/arch/rv64/interrupt.rs
+++ src/arch/rv64/interrupt.rs
@@ -2,6 +2,8 @@
 
 use crate::arch::def;
 use crate::io::{BaseIO, ScratchIO};
+use crate::{arch, println};
+use rv64::insn::{s, RegisterRW};
 
 const PLIC_BASE: BaseIO<u32> = BaseIO::new(def::PLIC as usize);
 const PLIC_MENABLE: ScratchIO<u32> = ScratchIO::new(def::PLIC as usize + 0x2000, 0x100);
@@ -36,4 +38,66 @@ pub fn plic_claim(hart: usize) -> u32 {
 /// tell the PLIC we've served this IRQ.
 pub fn plic_complete(hart: usize, irq: u32) {
     PLIC_SCLAIM.index(hart).write(irq);
+}
+
+/// Check if it's an external interrupt or software interrupt, and handle it.
+/// returns `2` if timer interrupt,
+/// - `1` if other device,
+/// - `0` if not recognized.
+fn dev_intr() -> i32 {
+    let scause = s::scause.read();
+    let hart = arch::cpuid();
+
+    if s::SCAUSE_INTERRUPT.get(scause) != 0 {
+        // Interrupt
+        use s::ScauseExceptInt;
+
+        let irq = plic_claim(hart);
+        match ScauseExceptInt::try_from(except as u8).unwrap_or(ScauseExceptInt::Reserved) {
+            ScauseExceptInt::SupervisorSoftwareInterrupt => {
+                // Software interrupt from a machine-mode timer interrupt,
+                // forwarded by timervec in kernelvec.S.
+                if hart == 0 {
+                    // TODO: clockintr();
+                }
+
+                // Acknowledge the software interrupt by clearing
+                // the SSIP bit in sip.
+                unsafe { s::sip.clear_mask(s::SIP_SSIP) };
+
+                return 2;
+            }
+            ScauseExceptInt::SupervisorExternalInterrupt => {
+                // This is a supervisor external interrupt, via PLIC.
+                // irq indicates which device interrupted.
+
+                match irq as usize {
+                    def::UART0_IRQ => {
+                        // TODO: uartintr();
+                        return 1;
+                    }
+                    def::VIRTIO0_IRQ => {
+                        // TODO: virtio_disk_intr();
+                        return 1;
+                    }
+                    irq => {
+                        println!("unexpected interrupt irq={}", irq);
+                    }
+                }
+                // The PLIC allows each device to raise at most one
+                // interrupt at a time; tell the PLIC the device is
+                // now allowed to interrupt again.
+                if irq != 0 {
+                    plic_complete(hart, irq);
+                }
+                return 1;
+            }
+            other => {
+                println!("unexpected interrupt scause={:x?}", other);
+            }
+        }
+    } else {
+        // Non-interrupt
+    }
+    return 0;
 }
